ebf27e4ff5edd354604136dc87ae671f84103666
LewiFZZ <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 16:12:36 +0100

    Se ha cambiado la separacion de los formularios

--- interaccionBD/ejecutarConsultas.php
+++ interaccionBD/ejecutarConsultas.php
@@ -10,7 +10,40 @@ $dbName = 'alumnosPruebas';
 $conexion = new mysqli($host, $user, $pw, $dbName);
 
 // Ejecutar la consulta
-$consulta = $_POST['consulta'];
-$resultado = $conexion->query($consulta);
+if (isset($_GET)) {
+    $operacion = $_GET['operacion'];
+    switch ($operacion) {
+        case 'mostrarAlumnos':
+            // Mostrar alumnos ordenados por nombre
+            $resultado = $conexion->query("SELECT nombre FROM alumnos ORDER BY nombre");
+            break;
+        
+        case 'contarNombres':
+            // Contar nombres únicos
+            $resultado = $conexion->query("SELECT COUNT(DISTINCT nombre) as Nombres FROM alumnos");
+            break;
+
+        case 'mostrarApellidos':
+            // Mostrar apellidos ordenados por DNI
+            $resultado = $conexion->query("SELECT apellido FROM alumnos ORDER BY dni");
+            break;
+
+        case 'mostrarNombresApellidosUnicos':
+            // Mostrar nombres y apellidos únicos
+            $resultado = $conexion->query("SELECT DISTINCT nombre, apellido FROM alumnos ORDER BY apellido");
+            break;
+
+        case 'mostrarTodo':
+            // Mostrar todos los datos
+            $resultado = $conexion->query("SELECT DISTINCT nombre, apellido, dni FROM alumnos ORDER BY nombre");
+            break;
+
+        default:
+            die("Operación no válida.");
+    }
+
+}else{
+    
+}
 
 
