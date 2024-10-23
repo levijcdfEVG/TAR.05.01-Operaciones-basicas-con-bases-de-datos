10d177cae990849f9ee5fef2e9e8abc6fb8d2fd1
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 11:17:40 +0100

    Se ha añadido una verificacion de conexion

--- ejecutarConsultas.php
+++ ejecutarConsultas.php
@@ -9,7 +9,12 @@ $dbName = 'alumnosEscuela';
 //Conectar a la base de datos
 $conexion = new mysqli($host,$user,$pw,$dbName);
 
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("Conexión fallida: " . $conexion->connect_error);
+}
+
+//Ejecutar las consultas
 $consulta = $_POST['consulta'];
-
-
 $resultado = $conexion->query($consulta);
+header("Location:mostrar.php?result=$resultado");
