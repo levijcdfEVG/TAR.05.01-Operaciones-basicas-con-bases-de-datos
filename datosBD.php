a8b34416dc724286d0128bff6cf07f9812ccb275
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 11:14:45 +0100

    Se ha añadido el mostrar.php y se ha enlazado las consultas que se quieren hacer con el formulario y la pagina de gestion de las consultas

--- datosBD.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = 'localhost';
-$user = 'root';
-$pw = '';
-$dbName = 'alumnosEscuela';
-
-//Conectar a la base de datos
-$conexion = new mysqli($host,$user,$pw,$dbName);
--- ejecutarConsultas.php
+++ ejecutarConsultas.php
@@ -1,3 +1,15 @@
 <?php
 
-require 'datosBD.php';
+
+$host = 'localhost';
+$user = 'root';
+$pw = '';
+$dbName = 'alumnosEscuela';
+
+//Conectar a la base de datos
+$conexion = new mysqli($host,$user,$pw,$dbName);
+
+$consulta = $_POST['consulta'];
+
+
+$resultado = $conexion->query($consulta);
