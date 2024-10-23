b7735c7bba44ea1b7a685139669fcb0c35f469b0
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 13:05:05 +0100

    Se ha cambiado la estructura del la pagina de mostrar

--- mostrar.php
+++ mostrar.php
@@ -10,31 +10,23 @@ require 'ejecutarConsultas.php';
         <title>Visualizar Consulta</title>
     </head>
     <body>
+        <h1>Resultado</h1>
         <div class="contendorCentrado">
-        <table>
-            <tr>
-                <th class="titulos">Nombre</th>
-                <th class="titulos">Apellido</th>
-                <th class="titulos">DNI</th>
-            </tr>
             <?php
                 // Mostrar resultados directamente sin foreach
                 while ($filaDeResultado = $resultado->fetch_assoc()) {   
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $filaDeResultado['nombre'];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $filaDeResultado['apellido'];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $filaDeResultado['dni'];
-                    echo "</td>";
-                    echo "</tr>";
+                    
+                    foreach ($filaDeResultado as $key => $value) {
+                        echo $key.":    ".$value.'<br>';
+                        var_dump($filaDeResultado);
+                        echo "<br>";
+                        
+                    }
+                    echo "------------------------------------------------------------";
+                    echo "<br>";
                 }
                 $conexion->close();
             ?>
-        </table>
     </div>
     </body>
 </html>
