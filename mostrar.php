edaadcb47fda75bdb5e1a0153cae1c1d2ec0a70b
Levi Josué Candeias de Figueiredo <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 11:26:58 +0100

    Se ha añadido el mostrar con su estructura

---
+++ mostrar.php
@@ -0,0 +1,21 @@
+<?php
+require 'ejecutarConsultas.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="css/index.css" />
+        <title>Visualizar Consulta</title>
+    </head>
+    <body>
+        <h1>Elige una Operación</h1>
+        <div class="contendorCentrado">
+            <?php
+                
+
+            ?>
+        </div>
+    </body>
+</html>
