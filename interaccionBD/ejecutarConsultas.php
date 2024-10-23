afa04edff36358e3bd883f82bef68812ff3af2de
LewiFZZ <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-23 16:24:27 +0100

    Se ha cambiado el manejo de los formularios

--- interaccionBD/ejecutarConsultas.php
+++ interaccionBD/ejecutarConsultas.php
@@ -8,9 +8,13 @@ $dbName = 'alumnosPruebas';
 
 // Conectar a la base de datos
 $conexion = new mysqli($host, $user, $pw, $dbName);
+// Inicializar variables
+$resultado = null;
+$operacion = null;
+$sql = "";
 
 // Ejecutar la consulta
-if (isset($_GET)) {
+if (isset($_GET['operacion'])) {
     $operacion = $_GET['operacion'];
     switch ($operacion) {
         case 'mostrarAlumnos':
@@ -42,8 +46,38 @@ if (isset($_GET)) {
             die("Operación no válida.");
     }
 
-}else{
+}else if (isset($_POST)) {
+    $operacion = $_POST['operacion']; // Recoge el value del botón submit
     
+    switch ($operacion) {
+        case 'insertar':
+            // Construir consulta de inserción
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $dni = $_POST['dni'];
+            $sql = "INSERT INTO alumnos (nombre, apellido, dni) VALUES ('$nombre', '$apellido', '$dni')";
+            echo "<h1>¡CONSULTA EXITOSA</h1>";
+            break;
+
+        case 'borrar':
+            // Construir consulta de borrado
+            $dni = $_POST['dni'];
+            $sql = "DELETE FROM alumnos WHERE dni = '$dni'";
+            echo "<h1>¡CONSULTA EXITOSA</h1>";
+            break;
+
+        case 'modificar':
+            // Construir consulta de modificación
+            $dni =$_POST['dni'];
+            $nuevoNombre = $_POST['nombre'];
+            $nuevoApellido = $_POST['apellido'];
+            $sql = "UPDATE alumnos SET nombre = '$nuevoNombre', apellido = '$nuevoApellido' WHERE dni = '$dni'";
+            echo "<h1>¡CONSULTA EXITOSA</h1>";
+            break;
+
+        default:
+            die("Operación no válida.");
+    }
 }
 
 
